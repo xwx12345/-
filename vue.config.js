0337201314cf80f6eb8d9ce85c789a60a5b1d1af
xwx12345 <1812299239@qq.com>
2023-01-03 00:49:11 +0800

    跨域问题1

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,26 @@
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//     configureWebpack:{
+//         devtool:'source-map'
+//     },
+//     transpileDependencies: true,
+//     lintOnSave: false,
+//     devServer: {
+//         proxy: {
+//             '/api/': {
+//                 target: 'https://localhost:8080/',
+//                 // target: 'http://124.220.158.211:5000/',
+//                 // ↑这个就是你的接口地址↑
+//                 changeOrigin: true,
+//                 pathRewrite: {
+//                     '^/api': '/api'
+//                 }
+//             }
+//         }
+//     }
+// })
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-    configureWebpack:{
-        devtool:'source-map'
-    },
-    transpileDependencies: true,
-    lintOnSave: false,
-    devServer: {
-        proxy: {
-            '/api/': {
-                target: 'https://localhost:8080/',
-                // target: 'http://124.220.158.211:5000/',
-                // ↑这个就是你的接口地址↑
-                changeOrigin: true,
-                pathRewrite: {
-                    '^/api': '/api'
-                }
-            }
-        }
-    }
+  transpileDependencies: true,
+  lintOnSave: false
 })
